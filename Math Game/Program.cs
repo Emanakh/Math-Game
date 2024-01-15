54d3e36d30a86f320b515072e2b51e4e71b0e129
Eman Alaa Khodary <emanalaakhodary@gmail.com>
2024-01-15 17:41:07 +0200

    updatingthe functions with stopwatch

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -1,13 +1,24 @@
-﻿namespace Math_Game
+﻿using System;
+using System.Diagnostics;
+
+
+namespace Math_Game
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            welcome();
+            Stopwatch stopwatch = new Stopwatch();
 
-            getName();
 
+            welcome(getName());
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+
+            Console.WriteLine("Enter a GAME number:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Substuction");
+            Console.WriteLine("3. muliplication");
+            Console.WriteLine("4. Quit From the Game");
             string choice = Console.ReadLine();
 
             switch (choice)
@@ -16,17 +27,15 @@
                     AdditionGame(" welcome to Addition game");
                     break;
                 case "2":
-                    Console.WriteLine("Substruction");
+                    SubstructionGame(" welcome to Substruction game");
                     break;
                 case "3":
-                    muliplicationGame("Multiplication");
+                    multiplicationGame("welcome to Multiplication game");
                     break;
                 case "4":
-                    Console.WriteLine("Divison");
-                    break;
-                case "5":
                     Console.WriteLine("END GAME");
                     break;
+
                 default:
                     Console.WriteLine("invalid input");
                     Environment.Exit(1);
@@ -40,14 +49,14 @@
                 return name;
             }
 
-            static void welcome()
+            static void welcome(string name)
             {
                 Console.Clear();
                 Console.Title = "math game";
 
                 Console.ForegroundColor = ConsoleColor.DarkCyan;
                 Console.WriteLine("********************************************");
-                Console.WriteLine("*      Welcome to " +
+                Console.WriteLine($"*      Welcome {name} to " +
                                      " the MATH GAME    *");
                 Console.WriteLine("********************************************");
                 Console.WriteLine();
@@ -58,20 +67,25 @@
                 Console.ResetColor();
             }
 
-
             static void AdditionGame(string msg)
             {
-                Console.WriteLine(msg);
+                Stopwatch stopwatch = new Stopwatch();
+
+                int levelRange = Level();
                 Random random = new Random();
                 int score = 0;
 
                 Console.WriteLine("enter number of rounds you want:");
                 int rounds = int.Parse(Console.ReadLine());
 
+                stopwatch.Start();
+
                 for (int i = 0; i < rounds; i++)
                 {
-                    var numOne = random.Next(1, 9);
-                    var numTwo = random.Next(1, 9);
+                    Console.Clear();
+                    Console.WriteLine(msg);
+                    var numOne = random.Next(1, levelRange);
+                    var numTwo = random.Next(1, levelRange);
 
                     Console.WriteLine($"{numOne} + {numTwo}");
                     Console.WriteLine("Enter your nswer");
@@ -82,28 +96,109 @@
                     {
                         if (result == (numTwo + numOne))
                         {
-                            Console.WriteLine("right answer");
+                            Console.WriteLine("right answer, press any key to the move next");
+                            Console.ReadLine();
                             score++;
                         }
                         else
                         {
-                            Console.WriteLine("wrong answer");
+                            Console.WriteLine("wrong answer, press any key to the move next");
+                            Console.ReadLine();
                         }
                     }
                     else
                     {
-                        Console.WriteLine("YOU didn't enter a number!");
+                        Console.WriteLine("YOU didn't enter a number! , your gained zero point for this question, press any key to move next");
+                        Console.ReadLine();
                     }
-                    if (i == rounds - 1) { Console.WriteLine($"GAME OVER , you score is {score} out of {rounds}"); }
+                    if (i == rounds - 1)
+                    {
+                        stopwatch.Stop();
+
+                        Console.WriteLine($" GAME END , you score is {score} out of {rounds} and total time you took to solve the questions is: {stopwatch.Elapsed.ToString(@"mm\:ss\.fff")}");
+
+                    }
                 }
 
 
             }
 
-
-            static void muliplicationGame(string msg)
+            static int[] GetDivisionNumbers(int levelRange)
             {
-                Console.WriteLine(msg);
+
+                Random random = new Random();
+                int numOne = random.Next(1, levelRange);
+                int numTwo = random.Next(1, levelRange);
+
+                var result = new int[2];
+
+                while (numOne % numTwo != 0)
+                {
+                    numOne = random.Next(1, levelRange);
+                    numTwo = random.Next(1, levelRange);
+
+                }
+                result[0] = numOne;
+                result[1] = numTwo;
+                return result;
+
+            }
+            static void SubstructionGame(string msg)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+
+                int levelRange = Level();
+
+                int score = 0;
+
+                Console.WriteLine("enter number of rounds you want:");
+                int rounds = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < rounds; i++)
+                {
+                    Console.Clear();
+                    Console.WriteLine(msg);
+
+
+                    int[] nummbers = GetDivisionNumbers(levelRange);
+
+                    Console.WriteLine($"{nummbers[0]} / {nummbers[1]}");
+                    Console.WriteLine("Enter your nswer");
+                    string answer = Console.ReadLine();
+
+                    int result;
+                    if (int.TryParse(answer, out result))
+                    {
+                        if (result == (nummbers[0] / nummbers[1]))
+                        {
+                            Console.WriteLine("right answer , press any key to the next question");
+                            score++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("wrong answer , press any key to the next question");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("YOU didn't enter a number! , your gained zero point for this question, press any key to the next question");
+                    }
+                    if (i == rounds - 1)
+                    {
+                        stopwatch.Stop();
+
+                        Console.WriteLine($" GAME END , you score is {score} out of {rounds} and total time you took to solve the questions is: {stopwatch.Elapsed.ToString(@"mm\:ss\.fff")}");
+                    }
+
+                }
+
+            }
+
+            static void multiplicationGame(string msg)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+
+                int levelRange = Level();
                 Random random = new Random();
                 int score = 0;
 
@@ -112,8 +207,10 @@
 
                 for (int i = 0; i < rounds; i++)
                 {
-                    var numOne = random.Next(1, 9);
-                    var numTwo = random.Next(1, 9);
+                    Console.Clear();
+                    Console.WriteLine(msg);
+                    var numOne = random.Next(1, levelRange);
+                    var numTwo = random.Next(1, levelRange);
 
                     Console.WriteLine($"{numOne} * {numTwo}");
                     Console.WriteLine("Enter your nswer");
@@ -124,19 +221,55 @@
                     {
                         if (result == (numTwo * numOne))
                         {
-                            Console.WriteLine("right answer");
+                            Console.WriteLine("right answer, press any key to the move next");
+                            Console.ReadLine();
                             score++;
                         }
                         else
                         {
-                            Console.WriteLine("wrong answer");
+                            Console.WriteLine("wrong answer, press any key to the move next");
+                            Console.ReadLine();
                         }
                     }
                     else
                     {
-                        Console.WriteLine("YOU didn't enter a number!");
+                        Console.WriteLine("YOU didn't enter a number! , your gained zero point for this question, press any key to move next");
+                        Console.ReadLine();
                     }
-                    if (i == rounds - 1) { Console.WriteLine($"GAME OVER , you score is {score} out of {rounds}"); }
+                    if (i == rounds - 1)
+                    {
+                        stopwatch.Stop();
+
+                        Console.WriteLine($" GAME END , you score is {score} out of {rounds} and total time you took to solve the questions is: {stopwatch.Elapsed.ToString(@"mm\:ss\.fff")}");
+                    }
+                }
+
+
+            }
+
+            static int Level()
+            {
+
+                Console.WriteLine("Enter the level you want:");
+                Console.WriteLine("1.Easy");
+                Console.WriteLine("2.intermediate");
+                Console.WriteLine("3.Hard");
+                Console.WriteLine("4.Extra Hard");
+                string level = Console.ReadLine();
+                switch (level)
+                {
+                    case "1":
+                        return 9;
+                    case "2":
+                        return 99;
+                        ;
+                    case "3":
+                        return 999;
+                    case "4":
+                        return 9999;
+                    default:
+                        Console.WriteLine("invalid input we will defaault set your level to easy ");
+                        return 9;
                 }
 
 
