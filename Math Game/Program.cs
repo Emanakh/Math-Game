04cd60d918f0f76168a9813990b16b2e2dd80fe8
Eman Alaa Khodary <emanalaakhodary@gmail.com>
2024-01-15 14:38:20 +0200

    welcome message

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -4,6 +4,21 @@
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            Console.Title = "math game";
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("********************************************");
+            Console.WriteLine("*      Welcome to " +
+                                 " the MATH GAME   *");
+            Console.WriteLine("********************************************");
+            Console.WriteLine();
+
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Explore a wide range of opperations. Happy gaming!");
+            Console.ResetColor();
+
             Console.WriteLine("Hello, World!");
         }
     }
